f567c7b1246a57c53408f9ce311ad7a21a818df2
Luis Miglietti <10-10459@usb.ve>
2014-10-27 18:16:40 -0430

    Se agregaron los parametros de la clase

--- Estacionamiento.py
+++ Estacionamiento.py
@@ -1,3 +1,6 @@
+#!/usr/bin/env python
+# -*- coding: utf_8 -*-
+
 '''
 Created on Oct 27, 2014
 
@@ -9,9 +12,17 @@ class Estacionamiento(object):
     classdocs
     '''
 
-
-    def __init__(self):
-        '''
-        Constructor
-        '''
+    def __init__(self,nombreDueno, nombreEst, direccionEst,
+                 telefono, correo, rif ):
         
+        self.nombreDueno=nombreDueno
+        self.nombreEst=nombreEst
+        self.direccionEst=direccionEst
+        self.telefono=telefono
+        self.correo=correo
+        self.rif=rif
+        
+           
+        
+        
+    
--- TestEstacionamiento.py
+++ TestEstacionamiento.py
@@ -4,13 +4,19 @@ Created on Oct 27, 2014
 @author: luis
 '''
 import unittest
+from Estacionamiento import Estacionamiento
 
 
 class Test(unittest.TestCase):
 
 
-    def testName(self):
+    def testInicial(self):
         pass
+    
+    def testParametros(self):
+        self.assertTrue(Estacionamiento("Luis Perez", "Estacionamiento CCS",
+                                        "Urb el placer, CC. Colinas del placer",
+                                        "J-2992823",04241033434,"SAGE@usb.ve"))
 
 
 if __name__ == "__main__":
