f443a0c7e4b9fe792bfd916733141212f98e6eff
Luis Miglietti <10-10459@usb.ve>
2014-10-27 17:47:24 -0430

    commit inicial

---
+++ Estacionamiento.py
@@ -0,0 +1,17 @@
+'''
+Created on Oct 27, 2014
+
+@author: luis
+'''
+
+class Estacionamiento(object):
+    '''
+    classdocs
+    '''
+
+
+    def __init__(self):
+        '''
+        Constructor
+        '''
+        
