5b2531661a70b8b802ac36a45f66aad905d242e1
luismiglietti <10-10459@usb.ve>
2014-11-12 12:04:43 -0430

    fin sprint 2

--- SAGE/views.py
+++ SAGE/views.py
@@ -7,12 +7,10 @@ Created on Nov 6, 2014
 
 @author: luis
 '''
-from re import match, search
-from Sist_SAGE.models import Estacionamiento
+from Sist_SAGE.models import  Estacionamiento
 from django.core.exceptions import ValidationError
-
+from Sist_SAGE.forms import EstacionamientoForm
 from django.http import HttpResponse
-import datetime
 from django.template import Context
 from django.template.loader import get_template
 from django.shortcuts import render
@@ -27,21 +25,18 @@ def menu (request):
     return render (request, 'menu.html')
 
 def crearEst(request):
-    nDueno=request.POST['nombreDueno']
-    nEst=request.POST['nombreEst']
-    dirEst=request.POST['direccionEst']
-    corr1=request.POST['correo_1']
-    corr2=request.POST['correo_2']
-    tel1=request.POST['telefono_1']
-    tel2=request.POST['telefono_2']
-    tel3=request.POST['telefono_3']
-    rif1=request.POST['rif']
-    p= Estacionamiento(nombreEst=nEst, nombreDueno=nDueno, direccionEst=dirEst,
-                       correo_1=corr1, correo_2=corr2, telefono_1=tel1,
-                        telefono_2=tel2, telefono_3=tel3, rif=rif1)
-    try:
-        p.clean_fields()
-    except ValidationError:
-        return render (request, 'error.html')
-    p.save()
-    return render (request, 'exito.html',{'est': p})
+    
+    args = {}
+    if request.method == 'POST':
+        form = EstacionamientoForm(request.POST)
+        if form.is_valid():
+            print form.is_valid()
+            form.save()
+            return render (request, 'exito.html',{'est': form})
+    else:
+        form = EstacionamientoForm()
+    args['form'] = form
+
+    return render(request,'Agregar.html',args)
+
+
--- Sist_SAGE/models.py
+++ Sist_SAGE/models.py
@@ -1,22 +1,23 @@
 #!/usr/bin/env python
 # -*- coding: utf_8 -*-
 
-from django.db import models
+from django import forms
 from django.core import validators
 from django.core.validators import EmailValidator, RegexValidator
+from django.core.exceptions import ValidationError
+from django.db import models
 
-# Create your models here.
-class Estacionamiento(models.Model):
-    
+class Estacionamiento (models.Model):
     nombreEst = models.CharField(max_length=50)
     nombreDueno = models.CharField(max_length=50)
     direccionEst = models.CharField(max_length=50)
     correo_1 = models.CharField(max_length=50, validators=[EmailValidator()])
-    correo_2 = models.CharField(max_length=50, blank=True)
-    telefono_1 = models.CharField(max_length=50,validators=[RegexValidator(regex='^0?[24]12-?[0-9]{7}$',message="Formato Invalido")])
-    telefono_2 = models.CharField(max_length=50,blank=True)
-    telefono_3 = models.CharField(max_length=50,blank=True)
+    correo_2 = models.CharField(max_length=50, blank=True, validators=[EmailValidator()])
+    telefono_1 = models.CharField(max_length=50,validators=[RegexValidator(regex='^0?(212|412|414|424|416|426)-?[0-9]{7}$',message="Formato Invalido, ej: 02121234567")])
+    telefono_2 = models.CharField(max_length=50, blank=True,validators=[RegexValidator(regex='^0?(212|412|414|424|416|426)-?[0-9]{7}$',message="Formato Invalido, ej: 02121234567")])
+    telefono_3 = models.CharField(max_length=50, blank=True,validators=[RegexValidator(regex='^0?(212|412|414|424|416|426)-?[0-9]{7}$',message="Formato Invalido, ej: 02121234567")])
     rif = models.CharField(max_length=50)
+    
     
     def __unicode__(self):              # __unicode__ on Python 2
         return (u"Dueño: "+self.nombreDueno+
